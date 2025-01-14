91388b98a7a26ed227c1c1cacd13a70d7c80fbdd
frrikk <frikk.a.odlolien@tietoevry.com>
2025-01-14 12:56:39 +0100

    feat: adds auto imports and import structure eslint config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
+import "./index.css";
 import * as React from "react";
 import { StrictMode } from "react";
-
 import { createRoot } from "react-dom/client";
-
 import App from "@/app";
-import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
